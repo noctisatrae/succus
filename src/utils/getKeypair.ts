16b1d0c8b281fbd2bd7051aa1e3aaf93497201a7
noctisatrae <pro.hector.kub@gmail.com>
2022-09-24 11:19:58 +0200

    bundled!

--- src/utils/getKeypair.ts
+++ src/utils/getKeypair.ts
@@ -1,5 +1,11 @@
 import { gun } from "../db";
 
+/**
+ *
+ * @async
+ * @param of For which address would you like to fetch the key.
+ * @returns ofKeypair The keypair of the address
+ */
 const getKeypair = async (of:string) => {
     let ofKeypair;
     await gun.get("keypairs").get(of).once(async keypair => {
